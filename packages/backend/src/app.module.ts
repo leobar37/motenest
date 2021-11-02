af9ad2816dd11cb756e1d891b7f5d1836acdcc83
leobar37 <leonardito3458@gmail.com>
2021-11-02 09:11:00 -0500

    chore : add typeorm config

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,8 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MeController } from './me/me.controller';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      password: 'alfk3458',
+      username: 'postgres',
+      host: '172.29.80.1',
+      port: 5432,
+      database: 'montenest',
+    }),
+  ],
   controllers: [AppController, MeController],
   providers: [AppService],
 })
